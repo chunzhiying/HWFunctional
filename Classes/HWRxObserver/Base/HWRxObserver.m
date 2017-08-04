58c56b49488c79029be542487700080ec1409177
chunzhiying <chun.zhiying.ggl@gmail.com>
2017-08-04 22:03:00 +0800

    Add dealloc log

--- Classes/HWRxObserver/Base/HWRxObserver.m
+++ Classes/HWRxObserver/Base/HWRxObserver.m
@@ -11,6 +11,12 @@
 #import "NSArray+FunctionalType.h"
 #import "NSObject+RxObserver.h"
 
+#ifdef DEBUG
+#define NSLog(fmt, ...) NSLog((@"[HWRxObserver]: " fmt), ##__VA_ARGS__)
+#else
+#define NSLog(...)
+#endif
+
 typedef NS_ENUM(NSUInteger, HWRxObserverType) {
     HWRxObserverType_UnKonwn, // no such property, observe failed
     HWRxObserverType_UnOwned, // created during Operating. AotuReleased when block over.
@@ -60,7 +66,7 @@ typedef NS_ENUM(NSUInteger, HWRxObserverType) {
 
 
 - (void)dealloc {
-    
+    NSLog(@"dealloc, [key : %@]", _keyPath);
 }
 
 - (void)onTap {
