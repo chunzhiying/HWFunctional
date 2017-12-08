0c310dcf8acccb09db74da3efe40dd1f4544684b
chunzhiying <chun.zhiying.ggl@gmail.com>
2017-12-08 11:24:28 +0800

    清掉调试代码

--- Demo/HWRxObserverDemo/ViewController.m
+++ Demo/HWRxObserverDemo/ViewController.m
@@ -14,16 +14,6 @@
 #import "HWRxVariable.h"
 #import <objc/runtime.h>
 
-@protocol bbp <NSObject>
-- (void)show;
-@end
-
-@interface Cbbp : NSObject <bbp>
-@end
-
-@implementation Cbbp
-@end
-
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *testBtn;
@@ -51,14 +41,7 @@
     }).forEach(HW_BLOCK(NSNumber *) {
         NSLog(@"filter %@", $0);
     });
-    
-    Cbbp *bb = [Cbbp new];
-    [self addMehod:bb];
-    if ([bb respondsToSelector:@selector(show)]) {
-        [bb show];
-    }
 
-    
 //
 //    _testBtn.rx_tap.subscribe(^(UIButton *button) {
 //        HWAnimInstance.scale(0.1, 2, 1, 1, 0.1).addTo(_testBtn.layer).run();
@@ -75,17 +58,17 @@
     
     
     
-//        @[[self after:1 result:YES flag:@"一"],
-//          [self after:3 result:YES flag:@"二"],
-//          [self after:1 result:YES flag:@"三"],
-//          [self after:2 result:YES flag:@"四"],
-//          [self after:1 result:NO flag:@"五"],
-//          [self after:9 result:YES flag:@"六"],
-//          [self after:1 result:YES flag:@"七"],
-//          [self after:1 result:NO flag:@"八"],
-//          [self after:1 result:YES flag:@"九"]].promise.complete(^(NSArray *results) {
-//              NSLog(@"全部完成:%@", results);
-//          });
+        @[[self after:1 result:YES flag:@"一"],
+          [self after:3 result:YES flag:@"二"],
+          [self after:1 result:YES flag:@"三"],
+          [self after:2 result:YES flag:@"四"],
+          [self after:1 result:NO flag:@"五"],
+          [self after:9 result:YES flag:@"六"],
+          [self after:1 result:YES flag:@"七"],
+          [self after:1 result:NO flag:@"八"],
+          [self after:1 result:YES flag:@"九"]].promise.complete(^(NSArray *results) {
+              NSLog(@"全部完成:%@", results);
+          });
     
 //        [self after:1 result:YES flag:@"一"]
 //        .next(^(id obj) {
@@ -133,12 +116,6 @@
 
 - (HWPromise *)after:(NSUInteger)time result:(BOOL)result flag:(NSString *)flag {
     HWPromise *promise = [HWPromise new];
-    
-//    if ([flag isEqualToString:@"二"]) {
-//        NSLog(@"%lu, %@, %@", (unsigned long)time, @(result), flag);
-//         promise.successObj = [NSString stringWithFormat:@"成功,%@" ,flag];
-//        return promise;
-//    }
    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(time * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"%lu, %@, %@", (unsigned long)time, @(result), flag);
